be65ef6de7bb1ea512f6abc19ce0de2c76542f53
Puranjay10 <puranjay.pkapoor@gmail.com>
2026-06-23 22:03:27 +0530

    Sale schema added

---
+++ backend/src/models/Sale.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const saleSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+
+        quantity: {
+          type: Number,
+          required: true,
+        },
+
+        sellingPrice: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    saleDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Sale",
+  saleSchema
+);

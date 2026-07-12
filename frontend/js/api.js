c10e64a56478324c01f682e410727a81c2679a63
Puranjay10 <puranjay.pkapoor@gmail.com>
2026-07-12 18:04:15 +0530

    Connected frontend to the deployed backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:5000/api";
-
+const API_BASE_URL =
+  "https://vyaaparbill-backend.onrender.com/api";
 class ApiClientError extends Error {
   constructor(message, options = {}) {
     super(message);
